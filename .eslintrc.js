7103c7d5e3a02e80fc9768c97c3bc2964ec19f9b
Juliana Bearzi <julietad299@gmail.com>
2022-04-25 17:10:00 -0300

    add lint, prettier and husky configuration & delete unused files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'react/prop-types': 'off',
+    'arrow-body-style': 'off',
+    'prefer-const': 'off',
+    'no-underscore-dangle': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'default-param-last': 'off',
+  },
+};
