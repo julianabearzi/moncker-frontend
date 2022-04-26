5b77ec8a6ae86826681d9ca1bdd3aa5e3d78c838
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-04-26 12:15:15 -0300

    Add container, and loginBox divs

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,5 +1,16 @@
+import './login.css';
+
 const Login = () => {
-  return <div>Login</div>;
+  return 
+  (
+    <div className="container">
+        <div className="logo">
+            <h1>Moncker App</h1>
+        </div>
+        <div class="loginBox">
+        </div>
+    </div>
+  )
 };
 
 export default Login;
