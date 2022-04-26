3ba1064f33a59d9f389402f1b6edd8adb5da61f1
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-04-26 19:06:38 -0300

    Add textField to Register component from Material Ui

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './register.css';
-
+import FormControl from '@mui/material/FormControl';
+import { TextField } from '@material-ui/core';
 
 const Register = () => {
   return( 
     <div className="container">
           <div className="registerBox">
-            asd
+            <h1>Register</h1>
+            <FormControl sx={{ width: '50ch' }}>
+            <TextField 
+              id="outlined-basic" 
+              label="Email" 
+              variant="outlined"
+              color="warning"
+              margin="dense"
+            />
+             <TextField 
+              id="outlined-basic" 
+              label="Repeat Email" 
+              variant="outlined"
+              margin="dense" 
+            />
+             <TextField 
+              id="outlined-basic" 
+              label="Password" 
+              variant="outlined"
+              margin="dense" 
+            />
+            </FormControl>
           </div>
     </div>
     )
