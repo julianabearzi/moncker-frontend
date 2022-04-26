6b510f7e40a99caaa028175559032c36ceffb34c
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-04-26 12:17:30 -0300

    Add Sign In and Sign Up divs

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -8,6 +8,27 @@ const Login = () => {
             <h1>Moncker App</h1>
         </div>
         <div class="loginBox">
+          <div class="signIn">
+                  <h1>Sign In</h1>
+                  <label for="username">Username</label>
+                  <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                  />
+                  <label for="password">Password</label>
+                  <input 
+                    type="text" 
+                    id="password" 
+                    name="password" 
+                  />
+                  <button>Sign In</button>
+              </div>
+              <div class="signUp">
+                  <h1>Welcome to login</h1>
+                  <h3>Don't have an account?</h3>
+                  <button class="btnSignUp">Sign In</button>
+              </div>
         </div>
     </div>
   )
