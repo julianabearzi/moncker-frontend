09eaa0de262c87434a30042d745aaa4815fa4e3b
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-05-13 21:39:06 -0300

    Create Expenses component

--- src/components/Expenses/index.jsx
+++ src/components/Expenses/index.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import LinearProgress from '@mui/material/LinearProgress';
+import Button from '../Shared/Button';
+import { userProfile as userProfileAction } from '../../redux/actions/profileActions';
+import { showModal as showModalAction } from '../../redux/actions/modalActions';
+import modalTypes from '../../redux/types/modalTypes';
+import ExpensesList from './ExpensesList';
+import ExpensesForm from './ExpensesForm';
+import ConfirmationMessage from './ConfirmationMessage';
+import Modal from '../Shared/Modal';
 
-const Expenses = () => {
-  return <div className="expensesTitle">Expenses</div>;
+const Expenses = ({
+  isLoading,
+  showModal,
+  modalType,
+  userProfile,
+  expenses,
+  // meta,
+}) => {
+  useEffect(() => {
+    userProfile();
+  }, []);
+
+  const showAddModal = () => {
+    showModal(modalTypes.ADD_EXPENSES;
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <LinearProgress color="success" />
+      ) : (
+        <div className="expensesContainer">
+          <Button btnLabel="Add Expenses" onClick={() => showAddModal()}>
+            Add Expenses
+          </Button>
+
+          <Modal>
+            {modalType === 'ADD_EXPENSES' && <ExpensesForm />}
+            {/* {modalType === 'DELETE_EXPENSES' && (
+              <ConfirmationMessage expensesId={meta.id} />
+            )} */}
+            {/* {modalType === 'UPDATE_EXPENSES' && <ExpensesForm inc={meta.inc} />} */}
+          </Modal>
+
+          <ExpensesList expenses={expenses} />
+        </div>
+      )}
+    </div>
+  );
+};
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      showModal: showModalAction,
+      userProfile: userProfileAction,
+    },
+    dispatch
+  );
 };
 
-export default Expenses;
+const mapStateToProps = (state) => ({
+  expenses: state.profile.expenses,
+  modalType: state.modal.modalType,
+  meta: state.modal.meta,
+  isLoading: state.profile.isLoading,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Expenses);
