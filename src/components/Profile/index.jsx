5d6adb5151c74c5f51eb2f728635c09a1d00e680
Juliana Bearzi <julietad299@gmail.com>
2022-05-17 17:29:40 -0300

    full profile component

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,11 +1,136 @@
+import { React, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { FiUser } from 'react-icons/fi';
+import { FaDollarSign } from 'react-icons/fa';
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
+import calTransaction from '../../utils/statistics';
+import subtract from '../../utils/subtraction';
+import DataGraph from './DataGraph';
+import UserProfileStats from './UserProfileStats';
+import { userProfile as userProfileAction } from '../../redux/actions/profileActions';
 import './profile.css';
 
-const Profile = () => {
+const Profile = ({ email, expenses, income, userProfile, profile }) => {
+  const [expResult, setExpResult] = useState([]);
+  const [incResult, setIncResult] = useState([]);
+  const [balanceResult, setBalanceResult] = useState([]);
+  useEffect(() => {
+    userProfile();
+  }, []);
+
+  useEffect(() => {
+    if (profile?.expenses) {
+      const expensesSum = calTransaction(profile?.expenses);
+      setExpResult(expensesSum);
+    }
+    if (profile?.income) {
+      const incomeSum = calTransaction(profile?.income);
+      setIncResult(incomeSum);
+    }
+    if (profile?.income && profile?.expenses) {
+      const expSum = calTransaction(profile?.expenses);
+      const incSum = calTransaction(profile?.income);
+      const calcBalance = subtract(incSum.sumTotal, expSum.sumTotal);
+      setBalanceResult(calcBalance);
+    }
+  }, [profile?.income]);
+  let navigate = useNavigate();
+  function handleClickExp() {
+    navigate('/expenses');
+  }
+  function handleClickInc() {
+    navigate('/income');
+  }
   return (
     <div>
-      <h2 className="profileTitle">Profile</h2>
+      <div className="profileContainer">
+        <div className="profileContainerUp">
+          <div className="profileContainerUpLeft">
+            <div className="dataUserContainer">
+              <h5>Profile</h5>
+              <Chip
+                sx={{
+                  color: 'white',
+                }}
+                label={email}
+                avatar={
+                  <Avatar
+                    sx={{
+                      textTransform: 'uppercase',
+                    }}
+                  >
+                    <FiUser
+                      style={{
+                        height: '20px',
+                        width: '20px',
+                      }}
+                    />
+                  </Avatar>
+                }
+              />
+            </div>
+            <div className="balanceContainer">
+              <h2 className="profileTitle">Balance</h2>
+              <FaDollarSign
+                style={{
+                  height: '25px',
+                  width: '25px',
+                  color: 'greenyellow',
+                }}
+              />{' '}
+              <p>{balanceResult?.subTotal}</p>
+            </div>
+          </div>
+          <div className="transactionsContainer">
+            <h2 className="profileTitle">Transactions</h2>
+            <DataGraph
+              income={incResult?.sumTotal}
+              expenses={expResult?.sumTotal}
+            />
+          </div>
+        </div>
+        <UserProfileStats
+          numOfTransExp={expenses?.length}
+          numOfTransInc={income?.length}
+        />
+        <div className="btnContainerProfile">
+          <button
+            className="customBtn btn1"
+            onClick={handleClickExp}
+            type="button"
+          >
+            <span>Expenses History</span>
+          </button>
+          <button
+            className="customBtn btn1"
+            type="button"
+            onClick={handleClickInc}
+          >
+            <span>Income History</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Profile;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      userProfile: userProfileAction,
+    },
+    dispatch
+  );
+};
+
+const mapStateToProps = (state) => ({
+  email: state.auth.email,
+  expenses: state.profile.expenses,
+  income: state.profile.income,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
